305486428f597e1fa5e0b8d20b6fbfa2c16143a1
Aziz <aziznal.dev@gmail.com>
2023-04-11 12:15:11 +0300

    Setup todo-details page template

--- src/app/screens/todo-details-page/todo-details-page.component.ts
+++ src/app/screens/todo-details-page/todo-details-page.component.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { todoCore } from '@core';
+import { Observable, tap } from 'rxjs';
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -12,16 +13,30 @@ import { CanDeactivateComponent } from '../../guards/can-decativate-component.te
 export class TodoDetailsPageComponent implements OnInit, CanDeactivateComponent {
   id!: number;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  state$?: Observable<todoCore.repositories.TodoState>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+
+    private getTodoByIdUsecase: todoCore.usecases.GetTodoByIdUsecase,
+  ) {}
 
   ngOnInit() {
-    this.id = this.route.snapshot.params['id'];
+    this.id = +this.route.snapshot.params['id'];
 
     if (this.id === null || this.id === undefined) {
       alert('Invalid todo id');
-
       this.router.navigateByUrl('/todo');
     }
+
+    this.state$ = this.getTodoByIdUsecase.execute(this.id).pipe(
+      tap(state => {
+        if (state.error) {
+          alert('Something went wrong. ' + state.error);
+        }
+      }),
+    );
   }
 
   canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
